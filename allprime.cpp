3899e1c898fe2ce9ccfa0ed72532be945f28fed2
Mohammad Aquib Khan <noorshab71@gmail.com>
2024-10-16 18:54:22 +0530

    commited

--- allprime.cpp
+++ allprime.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(){
+    system("cls");
+    string check;
     int num;
+    int i;
+    int j;
     cout<<"enter number to find prime numbers under  "<<endl;
     cin>>num;
-    
+    if(num<=2){
+        cout<<"no primes under "<<num<<endl;}
+        else{cout<<"number of primes under "<<num<<" are "<<endl;
+            for(i=3;i<num;i+=1){
+                for(j=2;j<i;j+=1){
+                    bool isprime=1;
+                    if(i%j==0){
+                        isprime=0;
+                        }else{(void)0;}
+                    if(isprime==1){
+                        cout<<i<<endl;}else{(void)0;}
+                    }
+                    
+                }
+
+            }
+
+        
+        return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+system("cls");
+string check;
+int num;
+int i;
+int j;
+cout<<"enter number to find prime numbers under "<<endl;
+cin>>num;
+if(num<=2){
+cout<<"no primes under "<<num<<endl;}
+else{cout<<"number of primes under "<<num<<" are "<<endl;
+for(i=3;i<num; i+=1){
+bool isprime=1;
+for(j=2;j<i;j+=1){
+if(i%j==0){
+isprime=0;
+break;
+}
+}
+if(isprime==1){
+cout<<i<<endl;
+}
+}
+}
+return 0;
+}

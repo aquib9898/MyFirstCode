01a948b98157ca9a3c32c62ea86911222ff4b305
Mohammad Aquib Khan <noorshab71@gmail.com>
2024-10-30 12:00:01 +0530

    commited

--- allprime.cpp
+++ allprime.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int num;
-    cout<<"enter number to find prime numbers under  "<<endl;
+    cout<<"enter number to find prime numbers under     "<<endl;
     cin>>num;
     
 }

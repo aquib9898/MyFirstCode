bf9fa822aaf973bc508e65b0606a0410a2b74abc
Mohammad Aquib Khan <noorshab71@gmail.com>
2024-11-17 01:22:02 +0530

    Commited

--- abd.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hell  world"<<endl;
-    return 0;
-}
--- allprime.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int num;
-    cout<<"enter  number to find prime numbers under      "<<endl;
-    cin>>num;
-    
-}
